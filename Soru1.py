f5b5ab1088a67ea306b366b484d78223e2fde306
anil <aaerciyas@gmail.com>
2021-05-25 22:37:23 +0300

    this project is finished too

--- Soru1.py
+++ Soru1.py
@@ -4,15 +4,13 @@ import numpy as np
 img = cv2.imread('Soru1.tif',0)
 kernel = np.ones((2,2),np.uint8)
 dilation = cv2.dilate(img,kernel,iterations = 1)
-opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 
-# cv2.imshow("dilation", dilation)
-# cv2.imshow("opening", opening)
-# cv2.imshow("closing", closing)
-# cv2.waitKey(0)
 
 output = cv2.connectedComponentsWithStats(dilation, connectivity=4)
 calculated = 31+26+25+27+21+20+26+23+6
-print("calculated: ", calculated)
-print(output[0])
+print("Number of characters count in the image: ", calculated)
+print("Connected component count from the image", output[0])
+
+cv2.imshow("image", dilation)
+cv2.imwrite("answer1.png",dilation)
+cv2.waitKey(0)
--- Soru2.py
+++ Soru2.py
@@ -1,20 +1,27 @@
 import cv2
 import numpy as np
-from numpy import ndarray
 
 img = cv2.imread('Soru2.tif',0)
 # global thresholding
 ret1,th1 = cv2.threshold(img,200,255,cv2.THRESH_BINARY)
+#show tresholded image
 cv2.imshow("threshold", th1)
+#create a 5x5 kernal component
 kernel = np.ones((5,5),np.uint8)
+# erode component 1 time and look for 8 connectivity
 erosion = cv2.erode(th1,kernel,iterations = 1)
-output = cv2.connectedComponentsWithStats(erosion, connectivity=4)
-
+output = cv2.connectedComponentsWithStats(erosion, connectivity=8)
+#counter for identifical pixel count
 count = 0
+#for each connected component
+print("Connected \t\t Num of\nComponent \t\t  Pixel")
 for i in range(output[0]-1):
-    print(output[2][i])
-    if output[2][i][4] > 300:
+    print(i+1, "\t\t\t\t", output[2][i][4])
+    # if pixel is bigger than it is identical
+    if output[2][i][4] > 100:
         count += 1
-print(count)
-cv2.imshow("asd",erosion)
+print("\n\nNumber of identical component count is ",count)
+cv2.imshow("erosion",erosion)
+cv2.imwrite("answer2-erosion.png", erosion)
+cv2.imwrite("answer2-threshold.png",th1)
 cv2.waitKey(0)
